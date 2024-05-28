8e833820e38d4b5c8be8a459acc89e3f90a010a5
gabigpalharini <gabigplharini@gmail.com>
2024-05-28 15:49:35 -0300

    28/05

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,5 @@ Route::post('animal/pesquisar/nome', [CadastroAnimalController::class, 'AnimalNo
 Route::post('animal/pesquisar/especie', [CadastroAnimalController::class, 'PesquisarporEspecie']);
 Route::post('animal/pesquisar/ra', [CadastroAnimalController::class, 'PesquisarporRa']);
 Route::delete('animal/excluir/{id}', [CadastroAnimalController::class, 'AnimalExcluir']);
-Route::put('animal/atualizar', [CadastroAnimalController::class, 'AtualizarAnimal']);
+Route::put('animal/atualizar/{id}', [CadastroAnimalController::class, 'AtualizarAnimal']);
 Route::get('animal/encontrar/{id}', [CadastroAnimalController::class, 'AnimalId']);
